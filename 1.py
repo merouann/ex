84c8a660cb349c82644a153189b0aa3d9fd2eff3
merouann <johnsmithx992@gmail.com>
2024-11-03 16:25:03 +0100

    1.py

---
+++ 1.py
@@ -0,0 +1,15 @@
+tableau1 = []
+tableau2 = []
+tableau3 = []
+
+for i in range(8):
+    n = int(input("donne un nombre n: "))
+    x = int(input("donne un nombre x: "))
+
+    tableau1.append(n)
+    tableau2.append(x)
+    tableau3.append(n + x)
+
+print("tableau1:", tableau1)
+print("tableau2:", tableau2)
+print("tableau3:", tableau3)
